a09868014943bd8f000701ad99dde6faae2bdd1e
incewoon <71214456+incewoon@users.noreply.github.com>
2026-06-24 18:06:46 +0900

    Setup Firebase App Check in MainActivity
    Initialize Firebase App Check with Play Integrity in MainActivity.

--- android/app/src/main/java/app/lovable/aialbum/MainActivity.java
+++ android/app/src/main/java/app/lovable/aialbum/MainActivity.java
@@ -1,5 +1,23 @@
 package app.lovable.aialbum;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Firebase App Check 초기화 (Play Integrity)
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance()
+        );
+    }
+}
